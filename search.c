9b8570916f6c36aa50f5a69b04095778c947a23f
Artur <magisterbrownie@gmail.com>
2024-06-12 19:30:11 +0000

    Search in a array tree

--- search.c
+++ search.c
@@ -2,13 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #define FIELD_X 7
 #define FIELD_Y 6
 #define FIELD_SZ FIELD_X*FIELD_Y
-//#define print_field(field) (for(intprintf("ss"))
+#define print_field(field) for(int i=0;i<FIELD_SZ;i++){printf("%d ", field[i]); if(i%FIELD_X==FIELD_X-1) printf("\n"); }
+
+#define DEPTH 3
+
+int ipow(int base, int exp)
+{
+    int result = 1;
+    for (;;)
+    {
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
+}
 
 int main(void) {
+    // Load board:
     printf("Search\n");
 
     uint8_t field[FIELD_SZ];
@@ -41,13 +61,36 @@ int main(void) {
         field[pt++] = fig;
     }
     
-
-    for(int i=0;i<FIELD_SZ;i++){
-        printf("%d ", field[i]);
-        if(i%FIELD_X==FIELD_X-1)
-            printf("\n");
-    }
+    print_field(field);
 
     if(f) fclose(f);
+
+    // Tree Search:
+    int tree = (ipow(FIELD_X, DEPTH)-1)/(FIELD_X-1);
+    uint8_t* fields = malloc(sizeof(uint8_t)*FIELD_SZ*tree);
+    float* votes = malloc(sizeof(float)*FIELD_SZ);
+    bool* explored = malloc(sizeof(bool)*FIELD_SZ);
+    explored[0] = true;
+    memcpy(fields, field, FIELD_SZ);
+
+    for(int d=0;d<DEPTH;d++){
+        for(int j=0;j<( ipow(FIELD_X, d)-1)/(FIELD_X-1);j++){
+            if(!explored[j] && explored[(j-1)/4]){
+                // Do a turn
+                memcpy(fields+j*FIELD_SZ, field, FIELD_SZ);
+                explored[j] = true;
+            }
+
+        }
+        int maxd = (ipow(FIELD_X, d)-1)/(FIELD_X-1);
+        printf("Tree %d\n", maxd);    
+    }
+
+    printf("Tree %d\n", tree);    
+    print_field(fields);
+
+    free(fields);
+    free(votes);
+    free(explored);
     return 0;
 }

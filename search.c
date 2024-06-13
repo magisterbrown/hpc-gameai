ce56eecc0c9e1ae1e40e8f55d477b5a709e8e5b8
Artur <magisterbrownie@gmail.com>
2024-06-13 20:45:09 +0000

    Childer are made

--- search.c
+++ search.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #define FIELD_X 7
 #define FIELD_Y 6
@@ -10,6 +11,16 @@
 #define print_field(field) for(int i=0;i<FIELD_SZ;i++){printf("%d ", field[i]); if(i%FIELD_X==FIELD_X-1) printf("\n"); }
 
 #define DEPTH 3
+
+#define QUEUE 50
+#define BATCH_SIZE 4
+
+
+struct Node {
+    uint8_t *field;
+    uint8_t figure;
+    struct Node *children[FIELD_X];
+};
 
 int ipow(int base, int exp)
 {
@@ -26,6 +37,8 @@ int ipow(int base, int exp)
 
     return result;
 }
+
+void expand(int size, uint8_t *batch, uint8_t *figure, uint8_t *result, bool *valid);
 
 int main(void) {
     // Load board:
@@ -60,37 +73,88 @@ int main(void) {
         }
         field[pt++] = fig;
     }
-    
-    print_field(field);
 
     if(f) fclose(f);
 
     // Tree Search:
-    int tree = (ipow(FIELD_X, DEPTH)-1)/(FIELD_X-1);
-    uint8_t* fields = malloc(sizeof(uint8_t)*FIELD_SZ*tree);
-    float* votes = malloc(sizeof(float)*FIELD_SZ);
-    bool* explored = malloc(sizeof(bool)*FIELD_SZ);
-    explored[0] = true;
-    memcpy(fields, field, FIELD_SZ);
+    struct Node queue[QUEUE];
+    uint8_t batch[FIELD_SZ*BATCH_SIZE];
+    uint8_t result[FIELD_SZ*BATCH_SIZE*FIELD_X];
+    uint8_t figure[BATCH_SIZE];
+    struct Node *batch_nodes[BATCH_SIZE];
+    bool valid[BATCH_SIZE*FIELD_X];
 
-    for(int d=0;d<DEPTH;d++){
-        for(int j=0;j<( ipow(FIELD_X, d)-1)/(FIELD_X-1);j++){
-            if(!explored[j] && explored[(j-1)/4]){
-                // Do a turn
-                memcpy(fields+j*FIELD_SZ, field, FIELD_SZ);
-                explored[j] = true;
-            }
+    memset(valid, false, BATCH_SIZE*FIELD_X);
 
-        }
-        int maxd = (ipow(FIELD_X, d)-1)/(FIELD_X-1);
-        printf("Tree %d\n", maxd);    
+    int head = 0;
+    int tail = 0;
+    struct Node root;
+    root.field = field;
+    root.figure = 1;
+
+    queue[tail++%QUEUE] = root;
+    assert(tail != head);
+    
+    int sz = 0;
+    for(int i=0;i<BATCH_SIZE;i++){
+        if(head == tail)
+            break;
+        sz++;
+        struct Node *first = queue+(head++%QUEUE);
+        batch_nodes[i] = first;
+        figure[i] = first->figure;
+        memcpy(batch+i*FIELD_SZ, first->field, FIELD_SZ);
     }
 
-    printf("Tree %d\n", tree);    
-    print_field(fields);
+    print_field(batch);
+    expand(sz, batch, figure, result, valid);
+    for(int i=0;i<sz*FIELD_X;i++){
+        if(!valid[i])
+            continue;
 
-    free(fields);
-    free(votes);
-    free(explored);
+        struct Node *new = malloc(sizeof(struct Node));
+        struct Node *parent = batch_nodes[i/FIELD_X];
+        new->figure = (2-(parent->figure>>1));
+        new->field = malloc(sizeof(uint8_t)*FIELD_SZ);
+        memcpy(new->field, result+i, FIELD_SZ);
+        uint8_t chid = i & ((1 << FIELD_X)-1);
+        parent->children[chid] = new;
+
+        printf("%d chid\n", chid);
+
+        queue[tail++%QUEUE] = *new;
+        assert(tail != head);
+
+    }
+    printf("\n");
+    print_field(result);
+    print_field(result);
+    
     return 0;
+}
+
+void expand(int size, uint8_t *batch, uint8_t *figure, uint8_t *result, bool *valid) {
+    for(int i=0;i<size;i++){
+        uint8_t *curr_field=batch+i*FIELD_SZ;
+        
+        for(int j=0;j<FIELD_X;j++){
+            if(curr_field[j] != 0)
+                continue;
+
+            valid[i*FIELD_X+j] = true;
+            uint8_t *res_field = result+i*FIELD_SZ*FIELD_X+j*FIELD_SZ;
+            memcpy(res_field, curr_field, FIELD_SZ);
+            
+            int cell, ncell;
+            for(int k=0;k<FIELD_Y-1;k++){
+                cell = j+FIELD_X*k;
+                ncell = j+FIELD_X*(k+1);
+                if(res_field[cell] == 0 && res_field[ncell] != 0)
+                    res_field[cell] = figure[i];
+            }
+            if(res_field[ncell] == 0)
+                res_field[j+(FIELD_Y-1)*FIELD_X] = figure[i];
+
+        }
+    }
 }
